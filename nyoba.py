51a26fe374f40c7a0959660d1927b5cde4b07c74
meisa <meisapl21@gmail.com>
2018-03-27 16:30:11 +0700

    edit

--- nyoba.py
+++ nyoba.py
@@ -1,12 +1,15 @@
 nama = input("Masukkan nama lengkap: ")
 kelas = input("Masukkan kelas")
 npm = input("Masukkan npm")
+jurusan = input("Masukan jurusan")
 tl = int(input("Masukkan tanggal lahir:"))
 umur = 2018 - tl
+alamat = input("Masukan alamat lengkap")
 print("Halo! ", nama)
 print("Kelasmu di ", kelas )
 print("NPMmu adalah ", npm)
+print("Jurusanmu adalah", jurusan)
 print("Umur kamu", umur)
-
-
-
+#serahhhh sheeeebbbbb nih kutambahin hoho
+print("Alamat kamu", alamat)
+print("Selesai :)")
